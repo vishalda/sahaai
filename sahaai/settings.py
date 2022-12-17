de02eafc29596c7cff3485df4cb4415d88585b70
vishalda <vdavishu@gmail.com>
2022-12-17 15:51:18 +0530

    Settings file update

--- sahaai/settings.py
+++ sahaai/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ngo'
+    'ngo',
+    'event',
+    'volunteer',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
