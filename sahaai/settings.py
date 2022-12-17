285d25a024136d9742723a203ad59e51f0b0b971
vishalda <vdavishu@gmail.com>
2022-12-17 15:33:25 +0530

    updated settings file

--- sahaai/settings.py
+++ sahaai/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ngo'
+    'corsheaders',
+    'ngo',
+    'event',
+    'volunteer'
 ]
 
 MIDDLEWARE = [
@@ -122,10 +125,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 AUTH_USER_MODEL = "ngo.Organization"
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
